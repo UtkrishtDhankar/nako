d20967dda1cc07b86ffb7161590ea65d58f5c38e
Utkarsh Kunwar <utkarsh2602@gmail.com>
2015-10-14 16:08:15 +0530

    Added init and snap to nako.c

--- nako.c
+++ nako.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "init.h"
-
 #include "snap.h"
 
+int print_usage()
+{
+		printf("nako <command> [arguments]\n");
+		return EXIT_SUCCESS;
+}
 int main(int argc, char *argv[])
 {
 	if (argc > 1) {
 		if (strcmp(argv[1], "init") == 0)
-			;
+			init_repo();
 		else if (strcmp(argv[1], "status") == 0)
 			;
 		else if (strcmp(argv[1], "stage") == 0)
@@ -16,17 +21,15 @@ int main(int argc, char *argv[])
 		else if (strcmp(argv[1], "destage") == 0)
 			;
 		else if (strcmp(argv[1], "snap") == 0)
-			;
+			snap_all("Test"); 
 		else if (strcmp(argv[1], "branch") == 0)
 			;
 		else if (strcmp(argv[1], "switch") == 0)
 			;
 		else if (strcmp(argv[1], "merge") == 0)
 			;
-		else if (strcmp(argv[1], "hello world") == 0)
-			;
 		else
-			printf("Wrong argument\n");
+			print_usage();
 	}
 	return EXIT_SUCCESS;
 }
