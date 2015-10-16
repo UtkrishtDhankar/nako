714116c2be325602d480d6ad8acdd66108340056
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2015-10-16 14:48:58 +0530

    Fixed massive memory leak in sha1.c
    Forgot to free the contents of the input file

--- sha1.c
+++ sha1.c
@@ -43,6 +43,7 @@ char *sha1(const char *file_name)
 
 	unsigned char *hash = malloc(SHA_DIGEST_LENGTH * sizeof(*hash));
 	SHA1(message, message_size_bytes, hash);
+	free(message);
 
 	char *hash_string = malloc(41 * sizeof(*hash_string));
 	create_hash_string(hash_string, hash);
