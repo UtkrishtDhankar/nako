491d903699650ce3940614b00b75dac8243fd5cc
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2015-10-16 14:31:25 +0530

    Added some basic checking for snap file

--- switch.c
+++ switch.c
@@ -2,5 +2,16 @@
 
 void switch_to_snap(const char *snap_hash)
 {
-	
+	char *snap_file_name;
+	if (asprintf(&snap_file_name, ".nako/snaps/%s", snap_hash) == -1)
+		exit(1);
+
+	FILE* snap_file = fopen(snap_file_name, "rx");
+	if (snap_file == NULL)
+		exit(1);
+
+	char ch;
+	while ((ch = getc(snap_file)) != EOF) {
+		putc(ch, stdout);
+	}
 }
