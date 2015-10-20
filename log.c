2a470fc9c16be7a62342370a43a5f17c5dc4bb24
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2015-10-20 21:46:53 +0530

    hanged log to use a struct to represent a snap

--- log.c
+++ log.c
@@ -1,5 +1,14 @@
 #include "log.h"
 
+struct snap {
+	FILE *file;     /* The file for the snap */
+
+	char *hash;     /* The hash of the snap */
+	char *message;  /* The message accompanying the snap. */
+	char *time_str; /* The string representation of the unix time
+	                   of the snap */
+};
+
 static inline int get_num_files(DIR *dirp)
 {
 	int num_files = 0;
@@ -23,34 +32,39 @@ void show_commits()
 
 	int num_files = get_num_files(snap_dir);
 
-	char **snap_hashes   = malloc(num_files * sizeof(*snap_hashes));
-	char **snap_messages = malloc(num_files * sizeof(*snap_messages));
-	char **snap_times    = malloc(num_files * sizeof(*snap_times));
+	struct snap *snaps = malloc(num_files * sizeof(*snaps));
 
 	int counter = 0;
 	while ((dp = readdir(snap_dir)) != NULL) {
 		if (dp->d_type == DT_REG) {
-			snap_hashes[counter] = NULL;
-			asprintf(&snap_hashes[counter], "%s", dp->d_name);
+			snaps[counter].hash = NULL;
+			asprintf(&snaps[counter].hash, "%s", dp->d_name);
 
 			char *full_file_name = NULL;
-			asprintf(&full_file_name, "%s/%s", snap_dir_name, dp->d_name);
+			asprintf(&full_file_name, "%s/%s", snap_dir_name,
+					dp->d_name);
 
-			FILE *snap_file = fopen(full_file_name, "r");
+			snaps[counter].file = fopen(full_file_name, "r");
 
 			free(full_file_name);
 
-			snap_messages[counter] = malloc(1024 * sizeof(*snap_messages[counter]));
-			snap_times[counter]    = malloc(11 * sizeof(*snap_times[counter]));
-			fscanf(snap_file, "%[^\n]s", snap_messages[counter]);
-			fscanf(snap_file, "%s", snap_times[counter]);
+			snaps[counter].message = malloc(1024
+				* sizeof(*snaps[counter].message));
+			snaps[counter].time_str = malloc(11
+				* sizeof(*snaps[counter].time_str));
 
-			printf("%s @ %s s - %s\n", snap_hashes[counter],
-				snap_times[counter], snap_messages[counter]);
+			fscanf(snaps[counter].file, "%[^\n]s",
+				snaps[counter].message);
+			fscanf(snaps[counter].file, "%s",
+				snaps[counter].time_str);
 
-			free(snap_times[counter]);
-			free(snap_messages[counter]);
-			free(snap_hashes[counter]);
+			printf("%s @ %s s - %s\n", snaps[counter].hash,
+				snaps[counter].time_str,
+				snaps[counter].message);
+
+			free(snaps[counter].time_str);
+			free(snaps[counter].message);
+			free(snaps[counter].hash);
 
 			counter++;
 		}
