31fb965ee57005da7350898be7d7ab0db2f3a2a3
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2015-10-22 07:53:07 +0530

    Added extracting object name and hash

--- switch.c
+++ switch.c
@@ -1,12 +1,34 @@
 #include "switch.h"
 
+#define KILOBYTE      1024
+#define HASH_STR_SIZE 40
+
 /*
  * Restores an object to the current directory
- * Here, snap_line is the snap of the
+ * Here, snap_line is the line containing the hash and the name of the object
  */
-static inline void restore_object(const char *snap_line)
+static void restore_object(const char *snap_line)
 {
+	char hash[HASH_STR_SIZE + 1];
 
+	sscanf(snap_line, "%s", hash);
+
+	/* Allocating 1 KB of memory for object name. Will reallocate more
+	   space if needed. */
+	char *object_name = malloc(KILOBYTE * sizeof(*object_name));
+
+	int snap_pos = HASH_STR_SIZE;
+	int name_pos = 0;
+	int name_size = KILOBYTE;
+	while(snap_line[snap_pos++] != '\0') {
+		object_name[name_pos++] = snap_line[snap_pos];
+		if (name_pos >= name_size) {
+			object_name = realloc(object_name,
+				name_size + KILOBYTE);
+		}
+	}
+
+	printf("HASH - %s\nNAME - %s", hash, object_name);
 }
 
 void switch_to_snap(const char *snap_hash)
